51402729b92dfe08aed844587fb21b825a360a02
alexander <alsoderg@student.jyu.fi>
2020-07-30 13:57:47 +0300

    1.11

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,6 +1,51 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const History = (props) => {
+  if (props.allClicks === 0) {
+    return (
+      <div>
+      no feedback given
+      </div>
+    )
+  }
+
+  return (
+    <div>
+    <Statistics value={[props.value[0], props.value[1] , props.value[2], props.value[3]]}/>
+    </div>
+  )
+}
+
+const Statistics = (props) => {
+  return ( 
+  <table>
+  <StatisticLine text="good" value ={props.value[0]} />
+  <StatisticLine text="neutral" value ={props.value[1]} />
+  <StatisticLine text="bad" value ={props.value[2]} />
+  <StatisticLine text="all" value ={props.value[3]} />
+  <StatisticLine text="average" value ={(props.value[0]*1 + props.value[1]*0 + props.value[2]*-1)/(props.value[3])} />
+  <StatisticLine text="positive" value ={(props.value[0]/ props.value[3] *100) + ' %'} />
+  </table>
+  )
+}
+
+const StatisticLine = (props) => {
+  return (
+  <tr>
+  <td>{props.text}</td> <td>{props.value}</td>
+  </tr>
+
+  )
+}
+
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}> 
+  {text}
+  </button>
+)
+
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -20,39 +65,17 @@ const App = () => {
     setBad(bad + 1)
   }
 
-  const Statistics = (props) => {
-    return (
-    <p> {props.text} {props.value}</p>
-    )
-  } 
-
-  const Button = ({onClick, text}) => (
-    <button onClick={onClick}> 
-    {text}
-    </button>
-  )
-
   return (
     <div>
       <h1>give feedback</h1>
         <Button onClick={clickGood} text='good'/>
         <Button onClick={clickNeutral} text='neutral'/>
         <Button onClick={clickBad} text='bad'/>
-      <h1>statistics</h1>
-        <Statistics text='good' value={good} />
-        <Statistics text='neutral' value={neutral} />
-        <Statistics text='bad' value={bad} />
-      <p>all {total} </p>
-      <p> average {(good*1 + neutral*0 + bad*-1)/(total)} </p>
-      <p> positive {(good / total *100)} %</p>
-
-      
+        <h1>statistics</h1> 
+        <History allClicks={total} value={[good, neutral, bad, total]}/>
     </div>
   )
 }
-
-
-
 
 ReactDOM.render(<App />, 
   document.getElementById('root')
